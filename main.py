e895f746d6de19210c9f5da194708a5b42f9eac0
technodog2000 <130249429+technodog2000@users.noreply.github.com>
2023-12-16 19:54:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,37 +3,45 @@ import requests
 import base64
 import io
 import PIL.Image
+from PIL import ImageTk
 from ttkthemes import ThemedTk
 import json
 import openai
 from tkinter import *
+from tkinter import ttk
 from tkinter.filedialog import askopenfile
-win = ThemedTk(theme="breeze")
+import sv_ttk
+win = Tk()
 win.title("open file")
-win.minsize(400,400)
+win.minsize(1,400)
+win.config(padx=20,pady=20)
 win.maxsize(800,900)
-openai.api_key = api_key = "YOUR_API_KEY"
+openai.api_key = api_key = "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"
 
 def file_retrieve():
-    global win
+    global win, newwin, buttony, brrr, br, image, buffered, base64_string, payload, api_url, response, json_data, la_lista, rhino
     file = askopenfile(filetypes=[("images",".png .jpg .jpeg")])
-    global newwin
+
     newwin = Toplevel(master=win)
     newwin.title("results")
     newwin.minsize(400,500)
-    global buttony
-    global brrr
-    brrr = PhotoImage(PIL.Image.open(file.name))
+
+    brrr = PIL.Image.open(file.name)
+    brrr = brrr.resize((100, 100))
+    br= ImageTk.PhotoImage(brrr)
     
 
-    global carto
-    carto = brrr.subsample(3,3)
-    buttony = Button(image=carto, master=newwin)
+
+
+    buttony = Canvas(master=newwin,height=100,width=100,borderwidth=0,highlightthickness=0)
+
+    rhino = buttony.create_image(50,50,image=br)
+    
     # else:
     #     buttony = Button(image=brrr, master=newwin)        
 
-    buttony.grid(row=1, column=0)
-    global image
+    buttony.grid(row=1, column=1)
+
     
     image = PIL.Image.open(file.name).convert("RGB")
     # Convert to a JPEG Buffer
@@ -67,20 +75,24 @@ def file_retrieve():
 
         messages = [
             {"role": "system", "content": "You are a carbon emmisions calculator. The user will give you a descriptions of 1 or more items and you will have to provide the emmisions for each item. Take into account that the words in between I think there are and and/or relate to a single item and are only describing it - they are not each individual items eg. chicken burger sandwich burger all mean the same. Only respond with a list format of each item - CO2 Emissions per annum in kg - eco alternative"},
-            {"role": "user", "content": f"{la_lista} tell me the emmisions on average over a year for each described iten. "}
+            {"role": "user", "content": f"{la_lista} tell me the emmisions on average over a year for each described item and put it in a ordered list. "}
         ]
     )
 
     print(response.choices[0].message.content)
     global labela
-    labela = Label(text=response.choices[0].message.content, font=("Arial", 16),master=newwin,wraplength=270)
-    labela.grid(row=2,column=0)
+    labela = ttk.Label(text=response.choices[0].message.content, font=("Arial", 8),master=newwin,wraplength=270)
+    labela.grid(row=1,column=1)
+
+    title = ttk.Label(text="Results",font=("Arial",40),master=newwin)
+    title.grid(row=0,column=0)
+
 
 
 giant = PhotoImage(file="image-icon (2).png")
-button = Button(image=giant, command=file_retrieve)
+button = ttk.Button(image=giant, command=file_retrieve)
 button.grid(column=1,row=1)
 
 
-
+sv_ttk.set_theme("light")
 win.mainloop()
